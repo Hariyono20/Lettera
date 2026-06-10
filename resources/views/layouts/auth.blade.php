1d725f704e190a1c5f19b9ae2e9bb95470297e4b
Hariyono20 <bentohariyono00@gmail.com>
2026-06-10 18:00:57 +0700

    Rapiin ui pimpinan

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -18,123 +18,63 @@
         {{-- ========================= --}}
         {{-- LEFT SIDE --}}
         {{-- ========================= --}}
-        <div class="hidden lg:flex w-1/2 relative bg-blue-20 items-center justify-center overflow-hidden">
+        <div class="hidden lg:flex w-1/2 relative bg-blue-50 items-center justify-center overflow-hidden">
 
-    {{-- Background Circular Lines --}}
-    <div class="absolute inset-0">
+            {{-- Background Circular Lines --}}
+            <div class="absolute inset-0">
+                {{-- Circle 1 --}}
+                <div class="absolute -left-40 top-1/2 -translate-y-1/2 w-[900px] h-[900px] rounded-full border border-blue-100"></div>
+                {{-- Circle 2 --}}
+                <div class="absolute -left-28 top-1/2 -translate-y-1/2 w-[760px] h-[760px] rounded-full border border-blue-100"></div>
+                {{-- Circle 3 --}}
+                <div class="absolute -left-16 top-1/2 -translate-y-1/2 w-[620px] h-[620px] rounded-full border border-blue-100"></div>
+                {{-- Circle 4 --}}
+                <div class="absolute -left-4 top-1/2 -translate-y-1/2 w-[480px] h-[480px] rounded-full border border-blue-100"></div>
+                {{-- Circle 5 --}}
+                <div class="absolute left-8 top-1/2 -translate-y-1/2 w-[340px] h-[340px] rounded-full border border-blue-100"></div>
+            </div>
 
-        {{-- Circle 1 --}}
-        <div
-            class="absolute -left-40 top-1/2 -translate-y-1/2 w-[900px] h-[900px] rounded-full border border-blue-100">
-        </div>
+            {{-- Soft Blur --}}
+            <div class="absolute top-10 left-10 w-72 h-72 bg-blue-200 rounded-full blur-3xl opacity-40"></div>
+            <div class="absolute bottom-10 right-10 w-72 h-72 bg-cyan-200 rounded-full blur-3xl opacity-40"></div>
 
-        {{-- Circle 2 --}}
-        <div
-            class="absolute -left-28 top-1/2 -translate-y-1/2 w-[760px] h-[760px] rounded-full border border-blue-100">
-        </div>
+            {{-- Main Content --}}
+            <div class="relative z-10 flex flex-col items-center w-full max-w-2xl px-6">
 
-        {{-- Circle 3 --}}
-        <div
-            class="absolute -left-16 top-1/2 -translate-y-1/2 w-[620px] h-[620px] rounded-full border border-blue-100">
-        </div>
+                {{-- Lottie Besar --}}
+                <div class="w-[700px] h-[700px] -my-20 flex justify-center items-center">
+                    <dotlottie-player
+                        src="{{ asset('images/3cc5f6fe-117f-11ee-8951-97f878212d11.lottie') }}"
+                        background="transparent"
+                        speed="1"
+                        style="width: 100%; height: 100%;"
+                        loop
+                        autoplay>
+                    </dotlottie-player>
+                </div>
 
-        {{-- Circle 4 --}}
-        <div
-            class="absolute -left-4 top-1/2 -translate-y-1/2 w-[480px] h-[480px] rounded-full border border-blue-100">
-        </div>
+                <p class="mt-2 text-center text-slate-600 text-lg font-medium leading-relaxed max-w-lg">
+                    Mempermudah pelayanan administrasi desa melalui
+                    sistem digital yang cepat, aman, transparan,
+                    dan dapat diakses kapan saja.
+                </p>
 
-        {{-- Circle 5 --}}
-        <div
-            class="absolute left-8 top-1/2 -translate-y-1/2 w-[340px] h-[340px] rounded-full border border-blue-100">
-        </div>
-
-    </div>
-
-    {{-- Soft Blur --}}
-    <div
-        class="absolute top-10 left-10 w-72 h-72 bg-blue-100 rounded-full blur-3xl opacity-50">
-    </div>
-
-    <div
-        class="absolute bottom-10 right-10 w-72 h-72 bg-cyan-100 rounded-full blur-3xl opacity-50">
-    </div>
-
-    {{-- Wave Divider --}}
-    <div class="absolute right-0 top-0 h-full w-[140px] z-20">
-
-        <svg
-            class="h-full w-full"
-            viewBox="0 0 140 1000"
-            preserveAspectRatio="none">
-
-            <path
-                fill="#ffffff"
-                d="
-                M0 0
-                C120 60,120 140,0 200
-                C120 260,120 340,0 400
-                C120 460,120 540,0 600
-                C120 660,120 740,0 800
-                C120 860,120 940,0 1000
-                L140 1000
-                L140 0
-                Z">
-            </path>
-
-        </svg>
-
-    </div>
-
-    {{-- Main Content --}}
-    <div class="relative z-10 flex flex-col items-center max-w-xl">
-
-    
-
-        {{-- Lottie Besar --}}
-        <div class="w-[650px] h-[650px] -my-16">
-
-            <dotlottie-player
-                src="{{ asset('images/3cc5f6fe-117f-11ee-8951-97f878212d11.lottie') }}"
-                background="transparent"
-                speed="1"
-                style="width:100%;height:100%;"
-                loop
-                autoplay>
-            </dotlottie-player>
+            </div>
 
         </div>
-
-        <p
-            class="mt-5 text-center text-slate-500 text-lg leading-relaxed max-w-md">
-            Mempermudah pelayanan administrasi desa melalui
-            sistem digital yang cepat, aman, transparan,
-            dan dapat diakses kapan saja.
-        </p>
-
-    </div>
-
-</div>
        
 
         {{-- ========================= --}}
         {{-- RIGHT SIDE --}}
         {{-- ========================= --}}
-        <div
-            class="w-full lg:w-1/2 bg-white flex items-center justify-center px-8 lg:px-20 relative">
+        <div class="w-full lg:w-1/2 bg-white flex items-center justify-center px-8 lg:px-20 relative">
 
             {{-- Background Soft --}}
-            <div
-                class="absolute top-0 right-0 w-80 h-80 bg-blue-100 rounded-full blur-3xl opacity-60">
-            </div>
-
-            <div
-                class="absolute bottom-0 left-0 w-80 h-80 bg-cyan-100 rounded-full blur-3xl opacity-60">
-            </div>
+            <div class="absolute top-0 right-0 w-80 h-80 bg-blue-100 rounded-full blur-3xl opacity-60"></div>
+            <div class="absolute bottom-0 left-0 w-80 h-80 bg-cyan-100 rounded-full blur-3xl opacity-60"></div>
 
             <div class="relative z-10 w-full max-w-md">
-
                 @yield('content')
-
             </div>
 
         </div>
